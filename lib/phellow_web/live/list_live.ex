dbfcb2cf8adc81021d880277f91a51de9a45f08f
Monte Johnston <johnston.monte@gmail.com>
2019-09-21 22:52:05 -0400

    Add archiving of cards

--- lib/phellow_web/live/list_live.ex
+++ lib/phellow_web/live/list_live.ex
@@ -7,6 +7,9 @@ defmodule PhellowWeb.ListLive do
       <div class="list-cards" id="<%= @dom_id %>" data-list-id="<%= @list_id %>" >
         <%= for card <- @cards do %>
           <div class="list-card" data-card-id="<%= card.id %>" data-list-id="<%= @list_id %>" >
+            <div  phx-click="archive_card" phx-value-card_id="<%= card.id %>">
+              <i class="material-icons delete-card">delete</i>
+            </div>
             <div class="list-card-details">
               <%= card.title %>
             </div>
@@ -21,6 +24,17 @@ defmodule PhellowWeb.ListLive do
 
     {:ok,
      assign(socket, cards: Content.cards_for_list(list_id), list_id: list_id, dom_id: dom_id)}
+  end
+
+  def handle_event("archive_card", %{"card_id" => id}, socket) do
+    card = Content.get_card!(id)
+
+    Phellow.Repo.transaction(fn ->
+      Content.delete_card(card)
+      Content.reorder_list_after_removing_card(card)
+    end)
+
+    {:noreply, assign(socket, lists: Content.lists_for_board(1), show_card_composer: 0)}
   end
 
   def handle_info(event, params) do
